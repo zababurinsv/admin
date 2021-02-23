5f35144a0496667fa49459123ec093436aff6bb8
Zababurin <s.zababurin.v@gmail.com>
2021-02-23 04:23:23 +0300

    ~

--- deploy.sh
+++ deploy.sh
@@ -8,21 +8,20 @@ cd ./docs
 npm run build
 
 # navigate into the build output directory
-cd /docs/vuepress/docs/src/.vuepress/dist
+cd ./src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+#git init
+#git add -A
+#git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git remote add Sergey https://github.com/zababurinsv/vuepress.git
-git config pull.rebase true
-git push -f --set-upstream Sergey master:gh-pages
-
+#git remote Sergey https://github.com/zababurinsv/vuepress.git
+#git push -f git@github.com:zababurinsv/zababurinsv.github.io.git/vuepress origin/gh-pages
+#git push --set-upstream https://github.com/zababurinsv/vuepress.git origin/gh-pages
 cd -
