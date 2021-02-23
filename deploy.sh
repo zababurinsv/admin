8c931c6db9b48bec5719bed14b483e0cab382adf
Zababurin <s.zababurin.v@gmail.com>
2021-02-23 04:25:55 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 # abort on errors
 set -e
-
+git add -A
+git commit -m 'update'
+git push
 # build
 cd ./docs
 npm run build
@@ -13,9 +15,9 @@ cd ./src/.vuepress/dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-#git init
-#git add -A
-#git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
@@ -23,5 +25,5 @@ cd ./src/.vuepress/dist
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 #git remote Sergey https://github.com/zababurinsv/vuepress.git
 #git push -f git@github.com:zababurinsv/zababurinsv.github.io.git/vuepress origin/gh-pages
-#git push --set-upstream https://github.com/zababurinsv/vuepress.git origin/gh-pages
+git push --set-upstream https://github.com/zababurinsv/vuepress.git origin/gh-pages
 cd -
