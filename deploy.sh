70721ca09c09ea1a2ae6bfb1f7841c4a1e886da9
Zababurin <s.zababurin.v@gmail.com>
2021-02-23 04:54:08 +0300

    update

--- deploy.sh
+++ deploy.sh
@@ -18,20 +18,13 @@ cd ./src/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-
-
 git remote add Sergey https://github.com/zababurinsv/vuepress.git
 
-git config pull.rebase false
-git pull https://github.com/zababurinsv/vuepress.git origin/master
-#git branch --set-upstream-to=Sergey origin/master
-#git push --set-upstream Sergey origin/master
-#git push --set-upstream Sergey origin/master
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-#git remote Sergey https://github.com/zababurinsv/vuepress.git
+#git config pull.rebase true
+#git config merge.pin.driver true
+#git pull --allow-unrelated-histories https://github.com/zababurinsv/vuepress.git origin/master
+#
+#git remote add Sergey https://github.com/zababurinsv/vuepress.git
 #git push -f git@github.com:zababurinsv/zababurinsv.github.io.git/vuepress origin/gh-pages
 #git push --set-upstream https://github.com/zababurinsv/vuepress.git origin/gh-pages
 cd -
