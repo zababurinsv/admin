91b9b90f09921e02eaba2313ccff560f3cb07b20
Zababurin <s.zababurin.v@gmail.com>
2021-03-08 21:15:59 +0300

    update

--- src/src/.vuepress/components/words.vue
+++ src/src/.vuepress/components/words.vue
@@ -12,7 +12,7 @@ export default {
     script.src = '/static/html/components/page-external/page-external.mjs'
     // let url = (location.hostname === 'localhost')?'http://localhost:5000':'https://tunnel-reverse.herokuapp.com'
     let url = 'https://tunnel-reverse.herokuapp.com'
-    this.$el.insertAdjacentHTML('beforeend',`<page-external data-url=${url} data-height=100></page-external>`)
+    this.$el.insertAdjacentHTML('beforeend',`<page-external data-url=${url} data-height=64></page-external>`)
     this.$el.appendChild(script)
   },
   methods: {
