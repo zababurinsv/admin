7a61a1e8d5458bfe50f1062b8ffecb5ad41375b6
Zababurin <s.zababurin.v@gmail.com>
2021-03-22 04:02:09 +0300

    update

---
+++ src/src/.vuepress/components/feed.vue
@@ -0,0 +1,55 @@
+<template></template>
+<script>
+export default {
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  mounted() {
+    let script = document.createElement('script')
+    script.type = 'module'
+    script.src = '/static/html/components/page-external/page-external.mjs'
+    // let url = (location.hostname === 'localhost')?'http://localhost:5000':'https://tunnel-reverse.herokuapp.com'
+    let url = 'https://server-feed.now.sh/'
+    this.$el.insertAdjacentHTML('beforeend',`<page-external data-url=${url} data-height=60></page-external>`)
+    this.$el.appendChild(script)
+  },
+  methods: {
+    set (event) {
+      axios.get(`${location.hostname}`).then((src)=>{
+        console.log('event:', src.data.message)
+      }).catch((e)=>{
+        console.log('error', e)
+      })
+    },
+    get (event) {
+      axios.get(`${location.hostname}`).then((src)=>{
+        console.log('event:', src.data.message)
+      }).catch((e)=>{
+        console.log('error', e)
+      })
+    },
+    del (event) {
+      axios.get(`${location.hostname}`).then((src)=>{
+        console.log('event:', src.data.message)
+      }).catch((e)=>{
+        console.log('error', e)
+      })
+    },
+    move (event) {
+      axios.get(`${location.hostname}`).then((src)=>{
+        console.log('event:', src.data.message)
+      }).catch((e)=>{
+        console.log('error', e)
+      })
+    }
+  }
+}
+</script>
+<style>
+.s__main {
+  display: flex;
+  height: 60vh;
+}
+</style>
---
+++ src/src/node/README.md
@@ -0,0 +1,18 @@
+# Nodes
+
+## Org
+<org class="s_main" />
+
+## Mongo
+<mongo class="s_main" />
+
+## News
+<feed class="s_main" />
+
+## Account
+<account class="s_main" />
+
+## WebDav
+<webdav-node class="s_main />
+
+
